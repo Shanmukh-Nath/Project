206738882f316a96601be76999c765e5ac65816d
Shanmukh Nath <shanmukh733@gmail.com>
2022-01-19 21:32:08 +0530

    second update

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function redirect(id){
+        if (id=='llogin'){
+                window.location.href="llogin.html"
+        }
+        else if(id=='lsignup'){
+                window.location.href="lsignup.html"
+        }
+        else if(id=='hlogin'){
+                window.location.href="hlogin.html"
+        }
+        else if(id=='hsignup'){
+                window.location.href="hsignup.html"
+        }
+}
